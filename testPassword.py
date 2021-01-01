f3c93c3c825723959a5e6f3114be49e5c470c240
meskigod <72917199+meskigod@users.noreply.github.com>
2021-01-01 21:52:46 +0100

    final changes

--- testPassword.py
+++ testPassword.py
@@ -1,7 +1,7 @@
 import unittest
 import Password
 
-class testPassword(unittest.TestCase):
+class TestPassword(unittest.TestCase):
     def test_hash_password_hash_check(self):
         hashed_pwd = Password.hash_password(self.password)
         self.assertTrue(Password.hash_check(self.password, hashed_pwd), (True))
